4feba5669c8e9c3f7114d52d16723aa23da730b1
Zeh237 <zehbrien@gmail.com>
2025-04-03 06:49:42 +0100

    Revert "first commit"
    This reverts commit 60350fae33379a449fd2f62ec96731df6756ca9e.

--- src/main/java/com/example/users/dto/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.users.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginDto {
-    private String username;
-    private String password;
-}
--- src/main/java/com/example/users/model/UserRole.java
+++
@@ -1,7 +0,0 @@
-package com.example.users.model;
-
-public enum UserRole {
-    USER,
-    MODERATOR,
-    ADMIN
-}
